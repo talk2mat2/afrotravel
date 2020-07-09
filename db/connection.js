0f0d4f4939d584448e871b53d43963d49f0cc101
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-07-09 06:06:29 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 var url = "mongodb://localhost:27017/martins"
-// const url ='mongodb+srv://Martins_c:chibuzo1@cluster0-gussd.mongodb.net/test?retryWrites=true&w=majority'
+// const url =`mongodb+srv://Martins_c:${password_here}@cluster0-gussd.mongodb.net/test?retryWrites=true&w=majority`
 
 const connectDB= async()=>{
  await mongoose.connect(url,{ useUnifiedTopology: true ,useNewUrlParser:true} );
